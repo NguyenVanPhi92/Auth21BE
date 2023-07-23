555519abf2e2914d5798e6a6495911ec30a98a8d
nvPhi@02 <nguyenvanphidmx123455@gmail.com>
2023-07-23 23:29:49 +0700

    commit

--- server.js
+++ server.js
@@ -1,38 +1,48 @@
-const cookieParser = require("cookie-parser");
-const userRoutes = require("./routes/userRoutes");
-const uploadRoutes = require("./routes/uploadRoutes");
-const mongoose = require("mongoose");
-const express = require("express");
-const app = express();
-require("dotenv").config();
+const cookieParser = require('cookie-parser')
+const userRoutes = require('./routes/userRoutes')
+const uploadRoutes = require('./routes/uploadRoutes')
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+const corsOptions = {
+    origin: true,
+    credentials: true
+}
+
+app.use(cors(corsOptions))
+require('dotenv').config()
 
 // db
 mongoose.connect(
-  process.env.MONGO_URL,
-  {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) throw err;
-    console.log("db connected");
+    process.env.MONGO_URL,
+    {
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    (err) => {
+        if (err) throw err
+        console.log('db connected')
 
-    const PORT = 8000;
-    app.listen(PORT, () => {
-      console.log("server is active");
-    });
-  }
-);
+        const PORT = 8000
+        app.listen(PORT, () => {
+            console.log('server is active')
+        })
+    }
+)
 
 // mw
-app.use(express.json()); // parser form => express
-express.urlencoded({ extended: true });
-app.use(cookieParser()); // parser cookies
+app.use(express.json()) // parser form => express
+express.urlencoded({ extended: true })
+app.use(cookieParser()) // parser cookies
 // public thư mục uploads
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads', express.static('uploads'))
 
 // routes
-app.use(userRoutes);
-app.use(uploadRoutes);
+app.use(userRoutes)
+app.use(uploadRoutes)
+
+module.exports = app
